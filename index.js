71b96e0c60da78a8e7a07a24fe6371d31ba467ad
louis rodriguez <125697808+LouisRodriguez12101815@users.noreply.github.com>
2024-04-13 22:51:02 -0400

    Create index.js
    insert of links that have influenced this project

---
+++ index.js
@@ -0,0 +1,32 @@
+//links that have influenced this project
+
+//https://youtu.be/W6NZfCO5SIk?si=EnhkfSWGE8rMY-9p
+
+//https://youtu.be/dX8396ZmSPk?si=PlQuYioH1LbIxs8R
+
+//https://youtube.com/shorts/wJcCfAgoraI?si=5Hsd8Bpyx5m6Ovsc
+//https://haveibeenpwned.com/
+
+// create anchor link element
+let link = document.createElement("a")
+
+// Create txt
+let txt = document.createTextNode("Medium Post")
+
+//append txt to anchor element
+link.appendChild(txt)
+
+// set the title
+link.title ="Medium Post";
+
+// set the href property
+link.href = "https://www.medium.com";
+
+// get text to add link to
+let el = document.getElementById("p")
+
+// append the link to the el id = "p"
+el.appendChild(link)
+<div class="textDescription">
+  <p id="p">blah blah blah blah blah blah blah blah blah blah </p>
+</div>
